2910a91713a65875fee9bec185eec2643e78ca2f
Michał Wojtaszek <modyhipi@gmail.com>
2019-10-06 02:41:40 +0200

    done all ex. 4

--- arraysEx.js
+++ arraysEx.js
@@ -42,22 +42,67 @@ for(let i = 0; i < 100; i++){
     //console.log(`length of array: ${lengthOfArray}`);
     for(let i2 = 0; i2 < lengthOfArray; i2++){
         bigArray.push(arrayMother[i][i2]);
-        //console.log(`next word of array: ${arrayMother[i][i2]}`);
+        //console.log(`next element of array: ${arrayMother[i][i2]}`);
     }
+}
+console.log(`Generated array ${bigArray}`);
+//done B.
+
+var bigArray2 = [...bigArray];
+
+function removeItems(arrayToRemove, isEven){
+    let index = 0;
+    let index2 = 0;
+    let lengthOfMyArray =  arrayToRemove.length;
+    
+    while(index < lengthOfMyArray){
+        if(isEven === false){
+            if( ( 
+                ( arrayToRemove[index - index2] % 2 ) !== 0 ) ||
+                ( arrayToRemove[index - index2] === 0 )
+              ) {
+                arrayToRemove.splice(index - index2, 1);
+                index2++;
+            }
+            index++;
+        }
+        else{
+            if( ( arrayToRemove[index - index2] % 2 ) === 0 ){
+                arrayToRemove.splice(index - index2, 1);
+                index2++;
+            }
+            index++;
+        }
+    }
+    return arrayToRemove;
 }
 
-console.log(`Generated array ${bigArray}`); 
-//done B.
-console.log(numberOfElements);
-let index = 0;
-let index2 = 0;
-while(index < numberOfElements){
-    if( ( bigArray[index - index2] % 2 ) !== 0 ){
-        bigArray.splice(index - index2, 1);
-        index2++;
-    }
-    index++;
-}
+bigArray = removeItems(bigArray, false);
+
 console.log(`Generated array - no odd numbers ${bigArray}`);
 //done C.
+
+bigArray.sort( function(a,b){ return a - b } );
+
+console.log(`Generated array - no odd numbers - ascending order ${bigArray}`);
+//done D.
+
+
+// Now there is a problem. If I am supposed to do it on the same array there will be no element left
+bigArray = removeItems(bigArray, true);
+
+console.log(`Generated array - no even numbers ${bigArray}`);
+
+bigArray.sort( function(a,b){ return a - b } );
+
+//or if i am supposed to do it once again on full array it will be as follow
+
+bigArray2 = removeItems(bigArray2, true);
+
+console.log(`Generated array - no even numbers ${bigArray2}`);
+
+bigArray2.sort( function(a,b){ return a - b } );
+
+console.log(`Generated array - no even numbers - ascending order ${bigArray2}`);
+//done E.
 
